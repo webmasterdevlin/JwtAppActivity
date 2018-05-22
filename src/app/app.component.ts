cbe63c489518ffe8d788dd08fe705ddac155df35
webmasterdevlin <webmasterdevlin@gmail.com>
2018-05-22 23:00:10 +0800

    completed working Angular client app with JWT.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AppUserAuth } from "./security/app-user-auth";
+import { SecurityService } from "./security/security.service";
 
 @Component({
-  selector: 'ptc-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "ptc-root",
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
-  title = 'ptc';
+  title: string = "Acme Training Company";
+  securityObject: AppUserAuth = null;
+  constructor(private _securityService: SecurityService) {
+    this.securityObject = _securityService.securityObject;
+  }
+
+  logout(): void {
+    this._securityService.logout();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list.component';
+import { ProductDetailComponent } from './product/product-detail.component';
+import { ProductService } from './product/product.service';
+import { CategoryService } from './category/category.service';
+import { CategoryListComponent } from './category/category-list.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './security/login.component';
+import {SecurityService} from './security/security.service';
+import {AuthGuard} from './security/auth.guard';
+import {RouterModule} from '@angular/router';
+import {HttpInterceptorModule} from './security/http-interceptor.module';
+import { HasClaimDirective } from './security/has-claim.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    CategoryListComponent,
+    DashboardComponent,
+    LoginComponent,
+    HasClaimDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpInterceptorModule
   ],
-  providers: [],
+  providers: [ProductService, CategoryService, SecurityService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
